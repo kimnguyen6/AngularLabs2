2bab1c3ba23d860d8339cbbbddb78bb785a8aee4
Kim Nguyen <kim.nguyen8@griffithuni.edu.au>
2019-08-02 09:10:14 +1000

    angular

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
+  users = [
+    {email: "pepe@thefrog.com", password: "feelsgoodman"},
+    {email: "pepe2@thefrog.com", password: "feelsbadman"},
+    {email: "pepe3@thefrog.com", password: "feelsokayman"}
+  ];
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  itemClicked(){
+    for(let i = 0; i < this.users.length; i++){
+      if (
+        this.email == this.users[i].email &&
+        this.password == this.users[i].password
+      )
+      {
+        this.router.navigateByUrl("/account")
+        alert("Login Success");
+      }
+      else {
+        alert("Login Unsuccessful");
+      }
+    }
+  }
+}
